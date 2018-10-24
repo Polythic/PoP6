eeca835737b3a9fc54b766e3c81f0b4407dad58a
Simon <simongram@me.com>
2018-10-24 15:23:35 +0200

    Update 6g0.fsx

--- 6g0.fsx
+++ 6g0.fsx
@@ -1,7 +1,8 @@
 let rec cfrac2float (lst:int list) : float =
   match lst with
     | [] -> 0.0
+    | [int] -> float lst.Head
     | _ -> float lst.Head + 1.0 / (cfrac2float(lst.Tail))
 
-let test = [1;2]
+let test = [1;2;3]
 printfn "%A" (cfrac2float test)

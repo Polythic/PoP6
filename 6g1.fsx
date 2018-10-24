87ee36af5e808d75c0768e82c73f4225bd41dde9
Simon <simongram@me.com>
2018-10-24 16:17:28 +0200

    Update 6g1.fsx

--- 6g1.fsx
+++ 6g1.fsx
@@ -1,2 +1,9 @@
 let rec float2cfrac (x: float) : int list =
-  
+  let qi = floor x
+  let ri = x-qi
+  printfn "%A" ri
+  match ri with
+    | 0.0 -> []
+    | _ -> int qi :: float2cfrac(1.0/ri)
+
+printfn "%A" (float2cfrac 3.245)

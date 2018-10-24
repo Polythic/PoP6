d14b1e0356b065cbfa97ae9d651a354f4fb1b580
Simon <simongram@me.com>
2018-10-24 20:26:05 +0200

    Update 6g2.fsx

--- 6g2.fsx
+++ 6g2.fsx
@@ -1,1 +1,9 @@
+let rec frac2cfrac (t:int) (n:int) : int list =
+  if n = 0 then
+    []
+  else
+    let qi = t / n
+    let ri = t % n
+    qi :: (frac2cfrac n ri)
 
+printfn "%A" (frac2cfrac 649 200)

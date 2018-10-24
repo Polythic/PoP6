0dd41e2ef2650261f7a887a7ff4066fd7a54133e
tangotroels <43818193+tangotroels@users.noreply.github.com>
2018-10-24 15:45:08 +0200

    Update 6g0.fsx

--- 6g0.fsx
+++ 6g0.fsx
@@ -4,5 +4,9 @@ let rec cfrac2float (lst:int list) : float =
     | [int] -> float lst.Head
     | _ -> float lst.Head + 1.0 / (cfrac2float(lst.Tail))
 
-let test = [1;2;3]
-printfn "%A" (cfrac2float test)
+let test0 = [1;2;3]
+let test1 = [9;1;2]
+let test2 = [6;3;1;1]
+printfn "%A" (cfrac2float test0)
+printfn "%A" (cfrac2float test1)
+printfn "%A" (cfrac2float test2)

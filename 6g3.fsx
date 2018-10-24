95b2144162499b002aecb01651efbd701deaa693
Simon <simongram@me.com>
2018-10-24 21:22:48 +0200

    Update 6g3.fsx

--- 6g3.fsx
+++ 6g3.fsx
@@ -4,6 +4,10 @@ let rec cfrac2frac (lst: int list) (i: int) : int * int =
     let ti = qi * 1 + 0
     let ni = qi * 0 + 1
     (ti,ni)
+  elif i = 1 then
+    let ti = qi * fst(cfrac2frac lst (i-1)) + 1
+    let ni = qi * snd(cfrac2frac lst (i-1)) + 0
+    (ti,ni)
   else
     let ti = qi * fst(cfrac2frac lst (i-1)) + fst(cfrac2frac lst (i-2))
     let ni = qi * snd(cfrac2frac lst (i-1)) + snd(cfrac2frac lst (i-2))

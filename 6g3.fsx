e25591a655961f21c21d81dc811eb9e081440144
Simon <simongram@me.com>
2018-10-24 21:17:32 +0200

    Update 6g3.fsx

--- 6g3.fsx
+++ 6g3.fsx
@@ -1,3 +1,12 @@
 let rec cfrac2frac (lst: int list) (i: int) : int * int =
-  let qi = lst.Head
-  
+  let qi = lst.[i]
+  if i = 0 then
+    let ti = qi * 1 + 0
+    let ni = qi * 0 + 1
+    (ti,ni)
+  else
+    let ti = qi * fst(cfrac2frac lst (i-1)) + fst(cfrac2frac lst (i-2))
+    let ni = qi * snd(cfrac2frac lst (i-1)) + snd(cfrac2frac lst (i-2))
+    (ti,ni)
+
+printfn "%A" (cfrac2frac [3;4;12;4] 3)

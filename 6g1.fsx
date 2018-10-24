7ffdb7177d3aee2d33ef65512d7b2892bea397a2
Simon <simongram@me.com>
2018-10-24 17:00:16 +0200

    Update 6g1.fsx

--- 6g1.fsx
+++ 6g1.fsx
@@ -4,6 +4,6 @@ let rec float2cfrac (x: float) : int list =
   printfn "%A" ri
   match ri with
     | 0.0 -> []
-    | _ -> int qi :: float2cfrac(1.0/ri)
+    | _ -> int qi :: float2cfrac(System.Math.Round(1.0/ri,7))
 
 printfn "%A" (float2cfrac 3.245)

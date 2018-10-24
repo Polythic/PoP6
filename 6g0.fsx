a98bc8d4e1ed63d377e588f04b0893853d0fd0d4
Polythic <43793756+Polythic@users.noreply.github.com>
2018-10-24 15:07:10 +0200

    Update 6g0.fsx

--- 6g0.fsx
+++ 6g0.fsx
@@ -1,1 +1,7 @@
+let rec cfrac2float (lst:int list) : float =
+  match lst with
+    | [] -> 0.0
+    | _ -> float lst.Head + 1.0 / (cfrac2float(lst.Tail))
 
+let test = [1;2]
+printfn "%A" (cfrac2float test)
